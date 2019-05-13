f85f16355c9a71e497fab6b9fbfcff29403753a8
Jiawei-Tang-0820 <jit072@ucsd.edu>
2019-05-13 12:14:07 -0700

    started gridworld

---
+++ src/gridworld.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class GridWorld extends React.Component {
+
+    
+
+    render() {
+        return(
+            <div className='fullscreen-background'>
+                <div className='side-panel'>
+
+                </div>
+                <div className='grid-world'>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GridWorld;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './home/home';
+import GridWorld from './gridworld';
 import * as serviceWorker from './serviceWorker';
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
-ReactDOM.render(<Home/>, document.getElementById('root'));
+ReactDOM.render(<GridWorld/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
